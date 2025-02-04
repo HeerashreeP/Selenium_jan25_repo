41bc1e352605f7a108feeedf54d8ae33244a0983
ADMIN <ADMIN@DESKTOP-8MO0VGR>
2025-02-04 11:09:20 +0530

    first commit

---
+++ SampleMaven_01/src/test/java/demosample/Addedforktest.java
@@ -0,0 +1,15 @@
+package demosample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Addedforktest {
+	
+	@Test
+	
+	public void add() {
+		
+		Reporter.log("Addedforktestnull",true);
+	}
+
+}
