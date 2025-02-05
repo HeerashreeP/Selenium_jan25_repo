d50ff335de1986ed7519ca52ff81355795667416
My PC <My PC@DESKTOP-R7D318I>
2025-02-05 10:24:07 +0530

    sync fork

---
+++ SampleMaven_01/src/test/java/syncFork/LearnSyncFork.java
@@ -0,0 +1,13 @@
+package syncFork;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LearnSyncFork {
+	@Test
+	public void syncFork() {
+		Reporter.log("To learn sync fork",true);
+		
+	}
+
+}
